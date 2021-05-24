1bb2a86f6d475ba85675ad485c746a30a32991de
Piyush Sehgal <piyush97sehgal@gmail.com>
2021-05-24 18:48:41 +0530

    common concepts

---
+++ index.js
@@ -0,0 +1,14 @@
+//common concepts
+//js is loosly coupled language or weakly coupled language
+var a;
+console.log(a);
+
+a = 10;
+console.log(a);
+
+a = "hello";
+console.log(a);
+
+//it is not invalid but it is not a good practice to set a variable as undefined
+a = undefined;
+console.log(a);
